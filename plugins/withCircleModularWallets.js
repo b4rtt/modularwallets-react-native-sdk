7ae32f5f1eb2f39501fdf091cb6a7e2e1be655d7
b4rtt <b4rt11@gmail.com>
2025-03-11 20:33:57 +0100

    Bump package version and update Expo plugin configuration

---
+++ plugins/withCircleModularWallets.js
@@ -0,0 +1,10 @@
+const { withPlugins } = require("@expo/config-plugins");
+
+const withCircleModularWallets = (config) => {
+  // Copy the implementation from the SDK's plugin
+  // ...
+
+  return config;
+};
+
+module.exports = withCircleModularWallets;
